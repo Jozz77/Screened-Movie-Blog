d798b0b42f8d18196e63015f4bc04e28e91098b9
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-09-27 18:41:33 +0100

    added comments to the posts views

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .models import Post, Comment , Contact
 
 # Create your views here.
+
+# about page
 def about(request):
     context = {
         'about':"active",
@@ -10,24 +12,28 @@ def about(request):
     }
     return render(request,"pages/about.html",context)
 
+# write for us page
 def write_for_us(request):
     context = {
         'write_for_us': "active" 
     }
     return render(request,"pages/write_for_us.html",context)
 
+# terms and conditions page
 def tos(request):
     context = {
         'tos': "active"
     }
     return render(request,"pages/tos.html",context)
 
+# privacy policy page
 def privacy(request):
     context = {
         'privacy': "active" 
     }
     return render(request,"pages/privacy.html",context)
 
+# contact us page
 def contact(request):
     context = {
         'contact':"active"
@@ -47,12 +53,15 @@ def contact(request):
     context['message'] = ""
     return render(request,"pages/contact.html",context)
 
+# error 404 page
 def error_404_view(request, exception):
     return render(request, '404.html')
 
+# error 500 page
 def error_500_view(request):
     return render(request, '500.html')
 
+# home page
 def home(request):
     posts = Post.objects.all()
     context = {
@@ -61,6 +70,7 @@ def home(request):
     }
     return render(request,"pages/home.html",context)
 
+# post detail page
 def post_detail(request, year, month, day, slug, author):
     post = Post.objects.get(slug=slug, author__username=author, publish__year=year, publish__month=month, publish__day=day)
     comments = Comment.objects.filter(post=post)
@@ -70,18 +80,21 @@ def post_detail(request, year, month, day, slug, author):
     }
     return render(request,"pages/blog_post.html",context)
 
+#  post movie category page
 def movies(request):
     context = {
         'movies':"active"
     }
     return render(request,"pages/movies_category.html",context)
 
+# randomgist post page
 def random_gist(request):
     context = {
         'random_gist':"active"
     }
     return render(request,"pages/randomGist_category.html",context)
 
+# new post page
 def post_new(request):
     context = {
         'post_new':"active"
