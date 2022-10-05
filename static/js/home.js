c8ce59bcf35f4f1914644cabc212d92d097e91b2
Tife <boluwatife1904@gmail.com>
2022-10-05 13:21:55 +0100

    changes to timeline and trailer section

--- static/js/home.js
+++ static/js/home.js
@@ -25,8 +25,12 @@ const timelineImgs = document.querySelectorAll(".timeline-image");
 const timelineContainer = document.querySelector(".timeline-container");
 const trailers = document.querySelectorAll(".trailer");
 const progressSteps = document.querySelectorAll(".progress-step");
+
 let imgNum = 0;
+let imgNumClick = 0;
+let imgNumClick_Constant = 0;
 let trailerNum = 0;
+let imgClicked = false;
 
 // getting index of each image on hover
 timelineImgs.forEach((img, index) => {
@@ -35,19 +39,40 @@ timelineImgs.forEach((img, index) => {
   });
 });
 
+// getting index of each image on click and updating progressbar
+timelineImgs.forEach((img, index) => {
+  img.addEventListener("click", () => {
+    imgNumClick = index;
+    imgClicked = true;
+    updateTimeline();
+    updateProgressbar();
+
+    // IMPLEMENTATION OF TRAILER SECTION
+    trailers.forEach((trailer, index) => {
+      index = imgNumClick;
+      trailerNum = index;
+
+      if (trailer.classList.contains("trailer-active")) {
+        trailer.classList.remove("trailer-active");
+      }
+    });
+    trailers[trailerNum].classList.add("trailer-active");
+  });
+});
+
 // updating timeline and progress bar on hover of each image
 timelineImgs.forEach((img) => {
   img.addEventListener("mouseenter", () => {
     updateTimeline();
-    updateProgressbar();
+    updateProgressbar_Hover();
   });
 });
 
-// removing progress bar and timeline on mouse out
+// updating progress bar and timeline on mouse out
 timelineImgs.forEach((img) => {
   img.addEventListener("mouseout", () => {
+    updateProgressbar();
     img.classList.remove("img-active");
-    // removeProgressBar();
   });
 });
 
@@ -66,7 +91,7 @@ function updateTimeline() {
 function updateProgressbar() {
   // implementation of bubble progression
   progressSteps.forEach((step, idx) => {
-    if (idx < imgNum + 1) {
+    if (idx < imgNumClick + 1) {
       step.classList.add("progress-step-active");
     } else {
       step.classList.remove("progress-step-active");
@@ -74,41 +99,41 @@ function updateProgressbar() {
   });
 
   // implementation of line progression
-  const progressActive = document.querySelectorAll(".progress-step-active");
+  let progressActive = document.querySelectorAll(".progress-step-active");
+
+  progress.style.width =
+    ((progressActive.length - 1) / (timelineImgs.length - 1)) * 100 + "%";
+}
+// updating progress bar on hover
+function updateProgressbar_Hover() {
+  // implementation of bubble progression
+  progressSteps.forEach((step, idx) => {
+    if (idx < imgNum + 1) {
+      step.classList.add("progress-step-active");
+    } else {
+      step.classList.remove("progress-step-active");
+    }
+  });
+  // implementation of line progression
+  progressActive = document.querySelectorAll(".progress-step-active");
+
   progress.style.width =
     ((progressActive.length - 1) / (timelineImgs.length - 1)) * 100 + "%";
 }
 
 // function removing progressbar
 function removeProgressBar() {
-  progressSteps.forEach((step) => {
-    step.classList.remove("progress-step-active");
-  });
-
-  progressSteps[0].classList.add("progress-step-active");
-  progress.style.width = "0%";
-}
-
-// IMPLEMENTATION OF TRAILER SECTION
-timelineImgs.forEach((img) => {
-  img.addEventListener("click", () => {
-    trailers.forEach((trailer, index) => {
-      index = imgNum;
-      trailerNum = index;
-      console.log(trailerNum);
-
-      if (trailer.classList.contains("trailer-active")) {
-        trailer.classList.remove("trailer-active");
+  if (!imgClicked) {
+    progressSteps.forEach((step, idx) => {
+      if (idx < imgNum + 1) {
+        step.classList.remove("progress-step-active");
       }
     });
 
-    trailers[trailerNum].classList.add("trailer-active");
-  });
-});
-
-// timelineImgs.forEach((img) => {
-//   img.addEventListener("click", () => {});
-// });
+    progressSteps[0].classList.add("progress-step-active");
+    progress.style.width = "0%";
+  }
+}
 
 /*added a click event listener to the images to make the trailer 
 box show when the image is clicked;
