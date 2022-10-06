37502dc3d213b484b3e488ddff5c22ad0196ffcc
Tife <boluwatife1904@gmail.com>
2022-10-06 16:05:40 +0100

    fully implemented search bar with clear button

--- static/js/header.js
+++ static/js/header.js
@@ -2,11 +2,17 @@ const modeToggle = document.querySelector(".mode-toggle");
 const moon = document.querySelector(".moon");
 const currentTheme = localStorage.getItem("theme");
 const currentIcon = localStorage.getItem("icon");
+const searchIcon = document.querySelector(".navigation__search");
+const searchIconMobile = document.querySelector(".navigation__search--mobile");
+const searchBar = document.querySelector(".search-bar");
+const clearBtn = document.querySelector(".clear-btn");
+const searchInput = document.querySelector(".search-bar__input");
+const overlay = document.querySelector(".overlay");
 
 if (currentTheme == "dark") {
   document.body.classList.add("lightmode");
 }
-if (currentIcon == 'moonIcon'){
+if (currentIcon == "moonIcon") {
   moon.classList.add("bi-sun");
 }
 
@@ -18,11 +24,42 @@ modeToggle.addEventListener("click", () => {
   if (document.body.classList.contains("lightmode")) {
     theme = "dark";
   }
-  let icon = 'sunIcon';
+  let icon = "sunIcon";
   if (moon.classList.contains("bi-sun")) {
-    icon = 'moonIcon';
+    icon = "moonIcon";
   }
 
   localStorage.setItem("theme", theme);
   localStorage.setItem("icon", icon);
+});
+
+// implementation of search bar
+searchIcon.addEventListener("click", () => {
+  searchBar.classList.toggle("search-active");
+  overlay.classList.toggle("active");
+});
+searchIconMobile.addEventListener("click", () => {
+  searchBar.classList.toggle("search-active");
+  overlay.classList.toggle("active");
+});
+
+// implementing clear button on search bar
+clearBtn.addEventListener("click", () => {
+  searchInput.value = "";
+});
+
+// implementing search bar remove on overlay click
+overlay.addEventListener("click", () => {
+  searchBar.classList.remove("search-active");
+  overlay.classList.remove("active");
+});
+
+// implementing search bar remove on enter
+searchInput.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13 && searchBar.classList.contains("search-active")) {
+    searchBar.classList.remove("search-active");
+  }
+  if (e.key === "Enter" && searchBar.classList.contains("search-active")) {
+    searchBar.classList.remove("search-active");
+  }
 });
