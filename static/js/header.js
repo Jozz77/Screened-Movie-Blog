36be772b0013cdcd85c729b42c41a394caff2348
Oyinade3 <oyinadehillary@gmail.com>
2022-10-06 01:26:39 +0100

    finished reset link page and icon change for themes

--- static/js/header.js
+++ static/js/header.js
@@ -1,17 +1,28 @@
 const modeToggle = document.querySelector(".mode-toggle");
+const moon = document.querySelector(".moon");
 const currentTheme = localStorage.getItem("theme");
+const currentIcon = localStorage.getItem("icon");
 
 if (currentTheme == "dark") {
   document.body.classList.add("lightmode");
 }
+if (currentIcon == 'moonIcon'){
+  moon.classList.add("bi-sun");
+}
 
 modeToggle.addEventListener("click", () => {
   document.body.classList.toggle("lightmode");
+  moon.classList.toggle("bi-sun");
 
   let theme = "light";
   if (document.body.classList.contains("lightmode")) {
     theme = "dark";
   }
-  
+  let icon = 'sunIcon';
+  if (moon.classList.contains("bi-sun")) {
+    icon = 'moonIcon';
+  }
+
   localStorage.setItem("theme", theme);
+  localStorage.setItem("icon", icon);
 });
