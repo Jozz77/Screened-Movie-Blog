8b77563f2531f51be013c50df4f0aed3118f4ff1
Tife <boluwatife1904@gmail.com>
2022-10-06 10:07:54 +0100

    added functionality of eye icons on the forms

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,92 +1,23 @@
-"use strict";
+const resetInput1 = document.querySelector("#password-reset-1");
+const resetInput2 = document.querySelector("#password-reset-2");
 
-const eyeIcon = document.querySelectorAll(".password-icon");
-const formInput = document.querySelectorAll(".form__input");
-const formInputActive = document.querySelectorAll(".input-active");
-const eyeIconActive = document.querySelectorAll(".icon-active");
-let eyeNum = 0;
-let inputNum = 0;
-let state = false;
+const eyeReset1 = document.querySelector("#eye-reset-1");
+const eyeReset2 = document.querySelector("#eye-reset-2");
 
-formInput.forEach((input, idx) => {
-  input.addEventListener("mouseenter", () => {
-    inputNum = idx;
-    updateInputField();
-    updateEyeIcon();
-  });
-});
-
-eyeIcon.forEach((icon, idx) => {
-  icon.addEventListener("mouseenter", () => {
-    eyeNum = idx;
-    updateInputField();
-    updateEyeIcon();
-  });
-});
-
-eyeIconActive.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    showPassword();
-  });
-});
-
-// getting indexes of icons and input elements
-// eyeIcon.forEach((icon, idx) => {
-//   idx = inputNum;
-//   eyeNum = idx;
-// });
-
-// eyeIcon[eyeNum].addEventListener("click", showPassword);
-
-// function showPassword() {
-//   console.log(inputNum);
-//   if (formInput[inputNum].type === "password") {
-//     formInput[inputNum].type = "text";
-//     console.log(formInput[inputNum].type);
-//   } else {
-//     formInput[inputNum].type = "password";
-//   }
-// }
-
-// const formInputActive = document.querySelectorAll(".input-active");
-
-function updateInputField() {
-  formInput.forEach((input) => {
-    if (input.classList.contains("input-active")) {
-      input.classList.remove("input-active");
-    }
-  });
-  formInput[inputNum].classList.add("input-active");
+function showPassword1() {
+  if (resetInput1.type === "password") {
+    resetInput1.type = "text";
+  } else {
+    resetInput1.type = "password";
+  }
+}
+function showPassword2() {
+  if (resetInput2.type === "password") {
+    resetInput2.type = "text";
+  } else {
+    resetInput2.type = "password";
+  }
 }
 
-function updateEyeIcon() {
-  eyeIcon.forEach((icon) => {
-    if (icon.classList.contains("icon-active")) {
-      icon.classList.remove("icon-active");
-    }
-  });
-
-  eyeIcon[eyeNum].classList.add("icon-active");
-}
-
-function showPassword() {
-  formInputActive.forEach((input) => {
-    console.log(input);
-    if (input.type === "password") {
-      input.type = "text";
-      // console.log(input.type);
-    } else {
-      input.type = "password";
-    }
-  });
-}
-
-// function hidePassword() {
-//   formInput.forEach((input) => {
-//     input.type = "password";
-//   });
-// }
-
-// eyeIcon.forEach((icon) => {
-//   console.log("hello");
-// });
+eyeReset1.addEventListener("click", showPassword1);
+eyeReset2.addEventListener("click", showPassword2);
