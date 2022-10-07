be168a21f18b27f23ee7ca23c513a668e52b7712
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-10-07 11:13:05 +0100

    implemented tagging in the latest post section

--- blog/urls.py
+++ blog/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', views.home, name='home'), # localhost:8000/
     path('post/<slug:author>/<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail'),
     path('post/new/', views.new_post, name='add_post'),
-    path('posts/category/<int:category>/', views.category, name='category'),
     path('posts/tag/<slug:tag_slug>/', views.tag, name='tag'),
+    path('posts/category/<int:category>/', views.category, name='category'),
     path('search/', views.search, name='search'),
     path('404/', views.error_404_view, name='error_404'),
 
