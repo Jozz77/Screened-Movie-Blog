1b5a75e9192206e7bd4869722333c48112db4393
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-10-13 16:21:10 +0100

    updated Procfile and settings

--- screened_movie_blog/settings.py
+++ screened_movie_blog/settings.py
@@ -53,13 +53,13 @@ AUTH_USER_MODEL = "user.CustomUser"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'screened_movie_blog.urls'
