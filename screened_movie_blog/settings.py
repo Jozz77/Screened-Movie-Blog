59c6a49634e8d25324a98ab392fe20fcca3b865d
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-10-13 15:41:27 +0100

    deployment update

--- screened_movie_blog/settings.py
+++ screened_movie_blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','localhost','127.0.0.1']
 
 
 # Application definition
@@ -156,7 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
@@ -215,3 +215,6 @@ SUMMERNOTE_CONFIG = {
     # and call `initSummernote()` on your page.
     "lazy": True,
 }
+
+
+
